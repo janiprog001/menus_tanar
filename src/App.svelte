7929d7784096a231a453795938f7c2a92cf84f87
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-19 12:39:56 +0200

    Menu

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+  import Masik from './lib/Masik.svelte'
+  var selected = 'Counter'
+</script>
+
+<main>
+  <div class="menu">
+    <button on:click={() => (selected = 'Counter')}>Counter</button>
+    <button on:click={() => (selected = 'mv')}>Másik valami</button>
+  </div>
+  <div>
+    {#if selected === 'mv'}
+      <Masik />
+    {:else if selected === 'Counter'}
+      <Counter />
+    {/if}
+  </div>
+</main>
+
+<style>
+  div.menu {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 20px;
+    background-color: #333;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  div.menu button {
+    font-size: 12px;
+    padding: 2px 20px;
+    margin: 2px;
+  }
+</style>
---
+++ src/lib/Masik.svelte
@@ -0,0 +1,3 @@
+<h2>
+    Másik valami
+</h2>
